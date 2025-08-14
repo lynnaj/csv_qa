de70244d0c043bb82ac052f2e9d0c9eba04c9673
Lynn Jirpongopas <ljirpong@gmail.com>
2025-08-14 09:39:03 -0700

    Update app.py
    langgraph app

--- app.py
+++ app.py
@@ -1,108 +1,50 @@
+import os
+from dotenv import load_dotenv
+
 import streamlit as st
-from langchain.chat_models import init_chat_model
-import pydantic
-import time
-import pandas as pd
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_experimental.tools import PythonAstREPLTool
-from langchain_core.output_parsers.openai_tools import JsonOutputKeyToolsParser
-import matplotlib.pyplot as plt
-from langchain.callbacks import StreamlitCallbackHandler
-import statsmodels.api as sm
+from langchain_core.messages import AIMessage, HumanMessage
 
-model_id = "amazon.nova-pro-v1:0"
+from graph import invoke_our_graph
+from st_callable_util import get_streamlit_cb  # Utility function to get a Streamlit callback handler with context
 
-df_azure = pd.read_csv("AzureUsage.csv")
-df_aws = pd.read_csv("AWSUsage.csv")[['Services', 'Regions', 'Costs', 'Dates']]
-df_aws['Dates'] = pd.to_datetime(df_aws['Dates'])
-df_azure['Date'] = pd.to_datetime(df_azure['Date'])
+load_dotenv()
 
-ls_aws_services = df_aws['Services'].unique().tolist()
-ls_azure_services = df_azure['ServiceName'].unique().tolist()
+st.title("☁💸 Cloud Cost Chatbot POC")
+st.markdown("#### Azure & AWS Cloud Cost Analysis with GPT-4o and LangGraph 🤖")
 
-# Initialize BedrockChat model
-llm = init_chat_model(
-    "amazon.nova-pro-v1:0",
-    model_provider="bedrock",
-    model_kwargs={'temperature': 0.7}
-)
+# st write magic
+"""
+I am using Streamlit UI and the official [`StreamlitCallbackHandler`](https://api.python.langchain.com/en/latest/callbacks/langchain_community.callbacks.streamlit.streamlit_callback_handler.StreamlitCallbackHandler.html) 
+within [_LangGraph_](https://langchain-ai.github.io/langgraph/) by leveraging callbacks in our 
+graph's [`RunnableConfig`](https://api.python.langchain.com/en/latest/runnables/langchain_core.runnables.config.RunnableConfig.html).
 
+---
+"""
 
-tool = PythonAstREPLTool(locals={"df_aws": df_aws, "df_azure": df_azure})
+if "messages" not in st.session_state:
+    # default initial message to render in message state
+    st.session_state["messages"] = [AIMessage(content="Ask me anything about AWS or Azure cloud costs!")]
 
-parser = JsonOutputKeyToolsParser(key_name=tool.name, first_tool_only=True)
-
-llm_with_tool = llm.bind_tools(tools=[tool], tool_choice=tool.name)
-
-df_template = """\`\`\`python
-str({df_name}.columns.tolist())
->>> {df_columns}
-\`\`\`"""
-
-df_context = "\n\n".join(
-    df_template.format(df_columns=str(_df.columns.tolist()), df_name=df_name)
-    for _df, df_name in [(df_aws, "df_aws"), (df_azure, "df_azure")]
-)
-
-system = f"""You have access to two datasets about Cloud Spend.  They are loaded as pandas dataframes, df_azure and df_aws. \
-Do not create your own sample datasets. \
-Here is a list of columns for each dataframe and the python code that was used to generate the list:
-{df_context} \
-Here is a list of AWS services: {ls_aws_services} \
-Here is a list of Azure services: {ls_azure_services} \
-Given a user question about the data, write the Python code to answer it.  Prioritize output the results as a dataframe. Avoid outputing strings.\
-Unless the user ask for a chart or graph, then use matplotlib for streamlit. \
-Be sure to include import matplotlib.pyplot as plt and end code with st.pyplot(fig)  \
-Don't assume you have access to any libraries other than built-in Python. To use pandas, please import pandas as pd. When aggregating over time, be sure to pass numeric_only=True . \
-For example, here is pandas code for aggregating spend by month over month: df_aws['Costs'].groupby(df_aws['Dates'].dt.to_period('M')).sum(numeric_only=True).reset_index()
-To forecast, please import statsmodels.api as sm. Note that disp is no longer support.  Use model.fit()\
-Make sure to refer only to the variables mentioned above."""
-
-prompt = ChatPromptTemplate.from_messages([("system", system), ("human", "{question}")])
-                                           #, MessagesPlaceholder(variable_name="chat_history")])
-
-chain = prompt | llm_with_tool | parser | tool
-
-st.title("Chat with a dataset")
-
-# Initialize chat history
-#if "messages" not in st.session_state:
-#    st.session_state.messages = []
-
-# Display chat messages from history on app rerun
-#for message in st.session_state.messages:
-#    with st.chat_message(message["role"]):
-#        st.markdown(message["content"], unsafe_allow_html=True)
-
-# Initialize chat history
-if "messages" not in st.session_state or st.sidebar.button("Clear conversation history"):
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
-
-# Display chat messages from history on app rerun
+# Loop through all messages in the session state and render them as a chat on every st.refresh mech
 for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
-    
-# Accept user input
-if query := st.chat_input("Ask me about any of the Cloud Spend Datasets"):
-    st.session_state.messages.append({"role": "user", "content": query})
-    # Display user message in chat message container
-    with st.chat_message("user"):
-        st.write(query, unsafe_allow_html=True)
-    # Add user message to chat history
-    #st.session_state.messages.append({"role": "user", "content": query})
+    # https://docs.streamlit.io/develop/api-reference/chat/st.chat_message
+    # we store them as AIMessage and HumanMessage as its easier to send to LangGraph
+    if type(msg) == AIMessage:
+        st.chat_message("assistant").write(msg.content)
+    if type(msg) == HumanMessage:
+        st.chat_message("user").write(msg.content)
 
-    # Display assistant response in chat message container
+# takes new input in chat box from user and invokes the graph
+if prompt := st.chat_input():
+    st.session_state.messages.append(HumanMessage(content=prompt))
+    st.chat_message("user").write(prompt)
+
+    # Process the AI's response and handles graph events using the callback mechanism
     with st.chat_message("assistant"):
-        st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
-        response = chain.invoke({"question": query, }, config={"verbose": True, "callbacks":[st_cb]}) #"chat_history": st.session_state.messages})
-        print("TYPE: " + str(type(response)))
-        if isinstance(response, str):
-            try:
-                response = st.pyplot(response)
-            except Exception as e:
-                st.error(f"Error displaying plot: {e}")
-                response = st.write(response)
-        else:
-            response = st.write(response)
-    # Add assistant response to chat history
-    #st.session_state.messages.append({"role": "assistant", "content": response})
+        msg_placeholder = st.empty()  # Placeholder for visually updating AI's response after events end
+        # create a new placeholder for streaming messages and other events, and give it context
+        st_callback = get_streamlit_cb(st.empty())
+        response = invoke_our_graph(st.session_state.messages, [st_callback])
+        last_msg = response["messages"][-1].content
+        st.session_state.messages.append(AIMessage(content=last_msg))  # Add that last message to the st_message_state
+        msg_placeholder.write(last_msg) # visually refresh the complete response after the callback container
